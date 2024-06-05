18ba8c313238095b0de216b2c9878195b1fe0394
Daile Alimo <daile.alimo1991@hotmail.com>
2024-06-05 20:35:39 +0100

    setup project structure with basic repository and model which can be expanded upon.

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/config"
 	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/net"
-	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/net/middleware"
+	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/net/routes"
 	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/persist"
+	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/repository"
 )
 
 func main() {
 	envConfig := config.NewEnvironmentConfiguration()
 
-	/* todo: database */
-	_, err := persist.NewDatabase(envConfig.Database)
+	// initialize database from environment configuration
+	database, err := persist.NewDatabase(envConfig.Database)
 	if err != nil {
 		panic(err)
 	}
@@ -21,12 +22,13 @@ func main() {
 	// Create a new instance of the appRouter
 	router := net.NewAppRouter()
 
-	// Register a GET route for the root URL ("/") with CORS middleware
-	router.Get("/", middleware.CorsMiddleware(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello, world!"))
-		},
-	), middleware.WideOpen)) // Use the WideOpen CORS options to allow unrestricted access
+	// initialize and mount routes
+	routes.NewUserRoutes(
+		router,
+		repository.NewUserRepository(
+			database,
+		),
+	)
 
 	// Start the HTTP server on port 8081 using the router
 	http.ListenAndServe(":8081", router)
