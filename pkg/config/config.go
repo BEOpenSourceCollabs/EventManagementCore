0147715e8ea0b94167781f6909125cf2d15f6ee3
awpt1mus <kamehamehabeech@gmail.com>
2024-06-08 13:53:59 +0530

    config: add port and env to configuration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,18 +2,32 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/persist"
 )
 
 // Configuration .
 type Configuration struct {
+	Env      GoEnv
+	Port     int
 	Database persist.DatabaseConfiguration
 }
 
 // NewEnvironmentConfiguration creates a configuration populated from os environment variables.
 func NewEnvironmentConfiguration() Configuration {
+
+	env := os.Getenv("ENV")
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+
+	if err != nil {
+		port = 8081
+	}
+
 	return Configuration{
+		Port: port,
+		Env:  ValidateEnv(GoEnv(env)),
 		Database: persist.DatabaseConfiguration{
 			Host:     os.Getenv("DATABASE_HOST"),
 			Port:     os.Getenv("DATABASE_PORT"),
