e415b6c8e72348d122ddbf999124240d432ec0d3
awpt1mus <kamehamehabeech@gmail.com>
2024-06-08 18:48:35 +0530

    add utils package
    - add utility functions to respond with unified json response
    - add utility function to read json payload
    - define validator instance, add some custom validations and error
      messages

---
+++ pkg/net/utils/json.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/BEOpenSourceCollabs/EventManagementCore/pkg/net/constants"
+)
+
+// reads json body into 'dest' and returns common errors,
+// make sure the `dest` is never nil otherwise it will panic
+func ReadJson(w http.ResponseWriter, r *http.Request, dest interface{}) {
+
+	if dest == nil {
+		panic(errors.New("must pass non nil pointer to decode json body into"))
+	}
+
+	//create reader with size limit
+	r.Body = http.MaxBytesReader(w, r.Body, constants.MAX_BODY_SIZE)
+
+	//decode json request body into dest struct
+	err := json.NewDecoder(r.Body).Decode(dest)
+
+	if err != nil {
+
+		var maxBytesError *http.MaxBytesError
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &maxBytesError):
+			WriteErrorJsonResponse(w, constants.ErrorCodes.BadRequest, http.StatusRequestEntityTooLarge, []string{"request body is too large"})
+			return
+		case errors.As(err, &syntaxError):
+			WriteErrorJsonResponse(w, constants.ErrorCodes.BadRequest, http.StatusBadRequest, []string{fmt.Sprintf("Body contains invalid/badly formed json at %d", syntaxError.Offset)})
+			return
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			WriteErrorJsonResponse(w, constants.ErrorCodes.BadRequest, http.StatusBadRequest, []string{"request body contains badly formed json"})
+			return
+		case errors.As(err, &unmarshalTypeError):
+			WriteErrorJsonResponse(w, constants.ErrorCodes.BadRequest, http.StatusBadRequest, []string{fmt.Sprintf("invalid field type in json body for %s", unmarshalTypeError.Field)})
+			return
+		case errors.Is(err, io.EOF):
+			WriteErrorJsonResponse(w, constants.ErrorCodes.BadRequest, http.StatusBadRequest, []string{"body cannot be empty"})
+			return
+		default:
+			WriteInternalErrorJsonResponse(w)
+			return
+		}
+
+	}
+
+}
