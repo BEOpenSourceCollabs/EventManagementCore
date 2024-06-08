927e2639e831940f97562530a6f9d13af8da6dbf
awpt1mus <kamehamehabeech@gmail.com>
2024-06-08 19:44:50 +0530

    fix: update register dto validation to not run validation if optional fields are empty

--- pkg/net/dtos/register.go
+++ pkg/net/dtos/register.go
@@ -3,7 +3,7 @@ package dtos
 type Register struct {
 	Email     string `json:"email" validate:"required,email,min=1,max=256"`
 	Password  string `json:"password" validate:"required,is-strong,min=6,max=50"`
-	FirstName string `json:"first_name" validate:"alpha,max=50"`
-	LastName  string `json:"last_name" validate:"alpha,max=50"`
+	FirstName string `json:"first_name" validate:"omitempty,alpha,max=50"`
+	LastName  string `json:"last_name" validate:"omitempty,alpha,max=50"`
 	Username  string `json:"username" validate:"required,max=20"`
 }
